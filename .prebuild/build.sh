65db4a819db0f393f538f7103e980a867746ae85
oznu <dev@oz.nu>
2021-02-05 21:20:58 +1100

    work arounds for node-abi problem

--- .prebuild/build.sh
+++ .prebuild/build.sh
@@ -9,10 +9,12 @@ echo "Building for $(uname -m)..."
 
 npm install --only=prod --ignore-scripts
 
-prebuild --all
+cp abi_registry.json $(npm -g prefix)/lib/node_modules/prebuild/node_modules/node-abi/
+
+prebuild -t 14.14.0
 
 if [ $ID = "debian" ] && [ "$(uname -m)" = "x86_64" ]; then
   echo "Building x86_64 32bit versions..."
   apt-get install -y g++-multilib gcc-multilib
-  prebuild --all -a ia32
+  prebuild -t 14.14.0 -a ia32
 fi
