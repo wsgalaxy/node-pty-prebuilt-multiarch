99bf7b03a03bb753a6b99633ca47e1bc8679f348
oznu <dev@oz.nu>
2021-02-05 23:12:09 +1100

    0.10.0 support, migrate to github actions

---
+++ .prebuild/build.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const child_process = require('child_process');
+
+// node-abi is still shipping the wrong data
+// correct this issue manually for now
+const prebuildPkgPath = path.dirname(require.resolve('prebuild'));
+const nodeAbiPkgPath = path.dirname(require.resolve('node-abi'));
+const prebuildPath = path.resolve(prebuildPkgPath, 'bin.js');
+const abiRegistryJsonPath = path.resolve(nodeAbiPkgPath, 'abi_registry.json');
+fs.copyFileSync(path.resolve(__dirname, 'abi_registry.json'), abiRegistryJsonPath);
+
+// common windows flags
+const winBuildOpts = [
+  '--include-regex',
+  '"\.(node|exe|dll|pdb)$"',
+]
+
+/**
+ * --------------- Node.js Build ---------------
+ */
+
+// define build targets
+const nodeBuildTargets = [
+  '-t',
+  '10.0.0',
+  '-t',
+  '11.0.0',
+  '-t',
+  ' 12.0.0',
+  '-t',
+  '13.0.0',
+  '-t',
+  '14.0.0',
+  '-t',
+  '15.0.0',
+]
+
+const nodeBuildCmd = [
+  prebuildPath,
+  ...nodeBuildTargets,
+]
+
+if (os.platform() === 'win32') {
+  nodeBuildCmd.push(...winBuildOpts);
+}
+
+console.log('Building for Node.js:');
+console.log(nodeBuildCmd.join(' '));
+
+child_process.spawnSync(process.execPath, nodeBuildCmd, {
+  stdio: ['inherit', 'inherit', 'inherit']
+});
+
+// build i386?
+if ((os.platform() === 'linux' && os.arch() === 'x64' && fs.existsSync('/usr/bin/apt')) || os.platform() === 'win32') {
+  nodeBuildCmd.push('-a', 'ia32');
+
+  child_process.spawnSync(process.execPath, nodeBuildCmd, {
+    stdio: ['inherit', 'inherit', 'inherit']
+  });
+}
+
+/** 
+ * --------------- Electron Build ---------------
+ */
+
+const electronBuildTargets = [
+  '-t',
+  '5.0.0',
+  '-t',
+  '6.0.0',
+  '-t',
+  '7.0.0',
+  '-t',
+  '8.0.0',
+  '-t',
+  '9.0.0',
+  '-t',
+  '10.0.0',
+  '-t',
+  '11.0.0',
+]
+
+const electronBuildCmd = [
+  prebuildPath,
+  '-r',
+  'electron',
+  ...electronBuildTargets,
+]
+
+if (os.platform() === 'win32') {
+  electronBuildCmd.push(...winBuildOpts);
+}
+
+console.log('Building for Electron:');
+console.log(electronBuildCmd.join(' '));
+
+child_process.spawnSync(process.execPath, electronBuildCmd, {
+  stdio: ['inherit', 'inherit', 'inherit']
+});
+
+if ((os.platform() === 'linux' && os.arch() === 'x64' && fs.existsSync('/usr/bin/apt')) || os.platform() === 'win32') {
+  electronBuildCmd.push('-a', 'ia32');
+
+  child_process.spawnSync(process.execPath, electronBuildCmd, {
+    stdio: ['inherit', 'inherit', 'inherit']
+  });
+}
--- .prebuild/build.sh
+++ .prebuild/build.sh
@@ -3,18 +3,10 @@
 . /etc/os-release
 
 cd /node-pty
-rm -rf node_modules
+# rm -rf node_modules
 
 echo "Building for $(uname -m)..."
 
-npm install --only=prod --ignore-scripts
+npm install --ignore-scripts
 
-cp abi_registry.json $(npm -g prefix)/lib/node_modules/prebuild/node_modules/node-abi/
-
-prebuild -t 14.14.0
-
-if [ $ID = "debian" ] && [ "$(uname -m)" = "x86_64" ]; then
-  echo "Building x86_64 32bit versions..."
-  apt-get install -y g++-multilib gcc-multilib
-  prebuild -t 14.14.0 -a ia32
-fi
+node .prebuild/build.js
